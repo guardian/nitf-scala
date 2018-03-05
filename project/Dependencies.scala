40ed4f7d5fa533480b9d9be5df641b9ddca2411d
Hosam Aly <hosam.aly.freelance@guardian.co.uk>
2018-03-05 10:37:09 +0000

    Create sbt build infrastructure
    The infrastructure is configured to generate projects dynamically.
    This makes it easier to add or remove supported versions as desired.

---
+++ project/Dependencies.scala
@@ -0,0 +1,24 @@
+import sbt._
+
+object Dependencies {
+  val scala212Version = "2.12.4"
+  val scala211Version = "2.11.12"
+  val scalaVersions = Seq(scala211Version, scala212Version)
+
+  val scalaXmlVersion = "1.1.0"
+  val scalaXml = "org.scala-lang.modules" %% "scala-xml" % scalaXmlVersion
+  val scalaParserCombinators = "org.scala-lang.modules" %% "scala-parser-combinators" % scalaXmlVersion
+
+  val xmlParsing = Seq(scalaXml, scalaParserCombinators)
+
+
+  val scalaTestVersion = "3.0.5"
+  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
+  val scalactic = "org.scalactic" %% "scalactic" % scalaTestVersion
+
+  val scalaCheck = "org.scalacheck" %% "scalacheck" % "1.13.4"
+
+  val xmlDiff = "com.github.andyglow" %% "scala-xml-diff" % "2.0.2"
+
+  val testing = Seq(scalaTest, scalaCheck, xmlDiff).map(_ % Test)
+}
